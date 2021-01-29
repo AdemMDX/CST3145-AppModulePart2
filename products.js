114e04c7618c561ad337cfb74511cf4d33f6d288
Adem Ali <aa4269@live.mdx.ac.uk>
2021-01-29 14:13:48 +0000

    added products for testing

---
+++ products.js
@@ -0,0 +1,102 @@
+let products = [
+    {
+        id: 1001,
+        title: "Maths",
+        description: "London",
+        price: "£" + 20,
+        image: "https://www.flaticon.com/svg/static/icons/svg/2891/2891382.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 3,
+    },
+    {
+        id: 1002,
+        title: "English",
+        description: "London",
+        price: "£" + 30,
+        image: "https://www.flaticon.com/svg/static/icons/svg/947/947540.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 4,
+    },
+    {
+        id: 1003,
+        title: "Science",
+        description: "London",
+        price: "£" + 15,
+        image: "https://www.flaticon.com/svg/static/icons/svg/841/841988.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 4,
+    },
+    {
+        id: 1004,
+        title: "Art",
+        description: "London",
+        price: "£" + 30,
+        image: "https://www.flaticon.com/svg/static/icons/svg/2970/2970729.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 5,
+    },
+    {
+        id: 1005,
+        title: "Dance",
+        description: "London",
+        price: "£" + 10,
+        image: "https://www.flaticon.com/svg/static/icons/svg/1997/1997669.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 3,
+    },
+    {
+        id: 1006,
+        title: "P.E.",
+        description: "London",
+        price: "£" + 15,
+        image: "https://www.flaticon.com/svg/static/icons/svg/3022/3022552.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 4,
+    },
+    {
+        id: 1007,
+        title: "Health",
+        description: "London",
+        price: "£" + 10,
+        image: "https://www.flaticon.com/svg/static/icons/svg/684/684262.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 2,
+    },
+    {
+        id: 1008,
+        title: "Music",
+        description: "London",
+        price: "£" + 25,
+        image: "https://www.flaticon.com/svg/static/icons/svg/651/651717.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 4,
+    },
+    {
+        id: 1009,
+        title: "Drama",
+        description: "London",
+        price: "£" + 15,
+        image: "https://www.flaticon.com/svg/static/icons/svg/860/860331.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 3,
+    },
+    {
+        id: 1010,
+        title: "Language",
+        description: "London",
+        price: "£" + 20,
+        image: "https://www.flaticon.com/svg/static/icons/svg/2104/2104994.svg",
+        //image2: "image 2 placeholder"
+        availableInventory: 10,
+        rating: 4,
+    },
+]
