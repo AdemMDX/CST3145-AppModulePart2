05fae1e52953082b2122d284fb1df5e7620f260f
Adem Ali <aa4269@live.mdx.ac.uk>
2021-02-12 16:42:29 +0000

    updated server access

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     // allow different header fields
     res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
 
